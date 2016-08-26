0832f74ada17d58a8ef47e120d0056697d94c2aa
malkasara <msy.03579866@gmail.com>
2016-08-26 03:16:34 +0300

    classes

---
+++ GroupBuying.DAL/GroupBuying.DAL/Model/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBuying.DAL.Model
+{
+   public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public virtual ICollection<Item> Items { get; set; }
+
+    }
+}
