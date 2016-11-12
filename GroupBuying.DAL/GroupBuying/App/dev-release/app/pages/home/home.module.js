e2def73ac08b63c99f6b47bc0de2f8886cb494cf
malkasara <msy.03579866@gmail.com>
2016-11-13 01:47:02 +0200

    erase extra pages, trying to add home page

---
+++ GroupBuying.DAL/GroupBuying/App/dev-release/app/pages/home/home.module.js
@@ -0,0 +1,20 @@
+﻿(function () {
+  'use strict';
+
+  angular.module('CollectiveP.pages.home', [])
+      .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig() {
+  $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'app/pages/home/home.html',
+            title: 'Home',
+          sidebarMeta: {
+            order: 800,
+          },
+        });
+  }
+
+})();
